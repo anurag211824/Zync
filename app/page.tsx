db422f046f95eab631c82c6e3ca95a60da5a2c16
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-07-26 01:07:37 +0530

    somya branch merge right bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import {prisma} from "@/prisma"
+export default async function Home() {
+  const users = await prisma.user.findMany()
+  console.log(users);
+  
   return (
     <div>
       Resize me to see breakpoints in action!
